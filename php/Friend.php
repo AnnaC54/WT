d8b18fadb99a0d7581816f1a45c31cbbe1b2de81
AnnaC54 <58769780+AnnaC54@users.noreply.github.com>
2021-12-14 22:38:46 +0100

    Teilaufgabe a), b)

---
+++ php/Friend.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Model;
+
+use JsonSerializable;
+
+class Friend implements JsonSerializable
+{
+    private $username;
+    private $status;
+
+    // ggf. weitere Attribute, z.B. description, layout optionen...
+
+    public function __construct($username = null)
+    
+    {
+        $this->username = $username;
+    }
+
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    public function jsonSerialize()
+    {
+        return get_object_vars($this);
+    }
+
+    public  function isAccepted()
+    {
+        $this->status = 'accepted';
+    }
+
+    public function isDismissed()
+    {
+        $this->status = 'dismissed';
+    }
+
+
+    public static function fromJson($data)
+    {
+        $user = new User();
+
+        foreach ($data as $key => $value) {
+            // verwendet key als Zeichenkette
+            // für den zugriff auf Attribute
+            $user->{$key} = $value;
+        }
+
+        return $user;
+    }
+    // public function toJson() { manuell, nicht nötig...!
+    //     return "{\"username\": \"$this->username\"}";
+    // }
+}
--- php/chat.php
+++ php/chat.php
@@ -1,4 +1,5 @@
 <?php
+require("start.php");
 ?>
 
 <!DOCTYPE html>
