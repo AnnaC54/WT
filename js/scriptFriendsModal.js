0b78f6903b00a775bde27097626d1cd5ec74591d
simong2709 <92425369+simong2709@users.noreply.github.com>
2021-12-19 16:47:31 +0100

    update friends

--- js/scriptFriendsModal.js
+++ js/scriptFriendsModal.js
@@ -2,6 +2,7 @@ let addFriend = document.getElementById("friendRequest");
 addFriend.addEventListener('click', function () {
 
     let friendName = document.getElementById("name-span").textContent;
+    //lert(friendName);
     let modalHeader = document.getElementById("modalFriendRequestHeader");
     modalHeader.innerText = "Friend Request from " + friendName;
 });
