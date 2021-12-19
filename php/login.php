046fc14d4f7fe5a077d9e6dc1e1a8bf547f9e14f
felixrcw <82609230+felixrcw@users.noreply.github.com>
2021-12-19 16:36:00 +0100

    Update login.php

--- php/login.php
+++ php/login.php
@@ -1,18 +1,19 @@
 <?php
 require("start.php");
-if(isset($_SESSION["user"])){
+if (isset($_SESSION["user"])) {
     header("Location: friends.php");
 }
-if (isset($_POST['username'])) {  //check if filled post form
-    $username = $_POST['username']; 
-    $password = $_POST['password']; 
-    if(\Utils\BackendService::login($username, $password)){//login with variables
-    $_SESSION["user"]=$username;
-    echo var_dump($_SESSION["user"]);
-    header("Location: friends.php");   //forward to friends like that???
+if (isset($_POST['login']) && !empty($_POST['username'] && !empty($_POST['password']))) {  //check if filled post form
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    echo $username . "," . $password;
+    if ($service->login($username, $password)) { //login with variables
+        $_SESSION["user"] = $username;
+        echo var_dump($_SESSION["user"]);
+        header("Location: friends.php");   //forward to friends like that???
+    }
 }
-}
-?>        
+?>
 
 
 <!DOCTYPE html>
@@ -22,13 +23,11 @@ if (isset($_POST['username'])) {  //check if filled post form
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <link rel="stylesheet" href="/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/style.css">
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
-        </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
 </head>
 <title>Profile</title>
 
@@ -40,21 +39,21 @@ if (isset($_POST['username'])) {  //check if filled post form
         </div>
         <div class="inputWidth p-3 m-3 rounded-3 bg-white justify-content-center">
             <h1 class="h1 mb-4 mt-2 fw-normal text-center">Please sign in</h1>
-                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">                        <!-- action="</?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>"  -->
-                    <div class="form-floating m-1">
-                        <input type="text" class="form-control" name="username"  id="username" placeholder="Username">
-                        <label for="floatingInput">Username</label>
-                    </div>
-                    <div class="form-floating m-1">
-                        <input type="password" class="form-control" name ="password" id="password" placeholder="Username">
-                        <label for="floatingInput">Password</label>
-                    </div>
-                </form>
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                <!-- action="</?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>"  -->
+                <div class="form-floating m-1">
+                    <input type="text" class="form-control" name="username" id="username" placeholder="Username">
+                    <label for="floatingInput">Username</label>
+                </div>
+                <div class="form-floating m-1">
+                    <input type="password" class="form-control" name="password" id="password" placeholder="Username">
+                    <label for="floatingInput">Password</label>
+                </div>
+            </form>
             <div class="m-3 text-center">
                 <a href="register.php">
                     <button class="btn btn-lg btn-secondary">Register</button></a>
-                <a href="friends.php">
-                    <button class="btn btn-lg btn-primary" type="submit">Sign in</button></a>
+                    <button class="btn btn-lg btn-primary" type="submit" name="login">Sign in</button>
             </div>
         </div>
     </div>
